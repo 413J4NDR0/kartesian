82e0af07b3c7bf33db65994e0016c0fb7e37c4d7
Alejandro Ortega <ortegaalejandro55@gmail.com>
2018-08-02 09:16:15 -0400

    Debugging

--- docs/js/dynamicMap-v2.js
+++ docs/js/dynamicMap-v2.js
@@ -1,5 +1,6 @@
 function main(regions, jsonData) {
     for (var j = 0; j < regions.length; j++) {
+        console.log(j)
         console.log(svgWrapper);
         console.log(svgDoc);
         /*var region = svgWrapper.getElementById(regions[j]);
