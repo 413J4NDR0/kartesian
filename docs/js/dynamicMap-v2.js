8b813eafde3f703f80ae7e557dc71d1efb4ae60c
Alejandro Ortega <ortegaalejandro55@gmail.com>
2018-08-02 15:44:11 -0400

    Added population statistics to firebase

--- docs/js/dynamicMap-v2.js
+++ docs/js/dynamicMap-v2.js
@@ -1,9 +1,11 @@
 function main(regions, jsonData) {
+    var sum = 0;
     for (var j = 0; j < regions.length; j++) {
         console.log(j)
         var entity = regions[j];
         var region = canvas.select('#' + entity);
         region.data('json', jsonData[entity]);
+        sum += jsonData[entity].pop_prewar - jsonData[entity].pop_2018;
         region.mouseover(function (e) {
             this.node.style.opacity = 0.65;
             setInfo(this);
@@ -16,9 +18,11 @@ function main(regions, jsonData) {
             this.node.style.opacity = 1;
         });
     }
-
+    console.log(sum)
 }
 
 function setInfo(region) {
-    document.getElementById('entity-name').innerHTML = region.data('json').entity;
+    var json = region.data('json');
+    document.getElementById('entity-name').innerHTML = json.entity;
+
 }
