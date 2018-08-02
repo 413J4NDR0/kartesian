e05ed57a10c118a428b841a1498eae60d57ca595
Alejandro Ortega <ortegaalejandro55@gmail.com>
2018-08-02 18:10:53 -0400

    Debugging colors via console

--- docs/js/dynamicMap-v2.js
+++ docs/js/dynamicMap-v2.js
@@ -29,8 +29,9 @@ function setInfo(region) {
 
 function protean(region, colorGradient) {
     var json = region.data('json');
-    var index = Math.round(json.pop_scaled / 0.0625)
+    var index = Math.round(json.pop_scaled / 0.0625);
+    console.log(colorGradient[index]);
     region.attr('fill', colorGradient[index]);
     region.attr('stroke', colorGradient[index]);
-
+    console.log(region.attr('fill'));
 }
