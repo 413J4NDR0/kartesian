4fa92b07c185386d84ed40b3a33b5e332b2908d9
Alejandro Ortega <ortegaalejandro55@gmail.com>
2019-04-22 00:27:25 -0400

    added color compatibility for real

--- docs/js/kartesian.js
+++ docs/js/kartesian.js
@@ -1,10 +1,12 @@
 var redGradient = ['#280303', '#360404', '#430606', '#510707', '#5e0808', '#6c0909', '#790a0a', '#870c0c',
 '#932424', '#9f3c3c', '#ab5454', '#b76d6d', '#c38585', '#cf9d9d', '#dbb6b6', '#e7cece', '#e7cece'];
 
-var blueGradient = ['#032828', '#043636', '#064343', '#510707', '#075151', '#096c6c', '#0a7979', '#0c8787',
+var tealGradient = ['#032828', '#043636', '#064343', '#510707', '#075151', '#096c6c', '#0a7979', '#0c8787',
 '#249393', '#3c9f9f', '#54abab', '#6db7b7', '#85c3c3', '#9dcfcf', '#b6dbdb', '#cee7e7', '#cee7e7'];
 
-function Kartesian(country, dataset=country,color=redGradient) {
+var gradients = new Map([['red' , redGradient], ['teal' , tealGradient]]);
+
+function Kartesian(country, dataset=country,color="red") {
     var svg_layout = '<object type="image/svg+xml" data="img/'+ country + '.svg" id="svg" style="max-height: inherit"></object>';
     $('#map-wrapper').prepend(svg_layout);
     var canvas;
@@ -51,7 +53,8 @@ function setInfo(region, jsonMeta) {
     document.getElementById('pop').innerHTML = jsonMeta.pop + ': ' + commafy(json.pop) ;
 }
 
-function protean(region, colorGradient) {
+function protean(region, color) {
+    var colorGradient = gradients.get(color);
     var json = region.data('json');
     var index = Math.round(json.pop_scaled / 0.0625);
     region.node.style.fill = colorGradient[index];
